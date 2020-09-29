ac5b1b9cf04a6278b9dd2394036a8f1ea483b471
rodrigo_borba <rodrigo.borba@jointecnologia.com.br>
2020-09-29 17:12:52 -0300

    teste axios OK

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,58 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
-
-
-class MyButton extends Component{
-  render(){
-    return <button onclick={() =>{ this.props.handleClick(this.props.label) }}
-    > 
-      {this.props.label}
-    </button>
-  }
-
-}
-
-class MyLabel extends Component{
-  render(){
-    console.log(this.props);
-    return <p>Antes da props: {this.props.text}</p>
-
-  }
-}
 
 
 class App extends Component{
   
   constructor(props){
     super(props);
-    
+
     this.state = {
-      labelText : '',
-      color: props.color,
-      initialColor: props.color,
-    };
-
-    this.setLabelText = this.setLabelText.bind(this);
+      data:[]
+    }
 
    }
 
-   setLabelText(labelText){
-     this.setState({labelText});
-   }
+  componentDidMount(){
+    axios.get('https://api.github.com/repositories')
+      .then(({data})=>{
+        this.setState({data});
+      })    
+  } 
 
   render(){
+    const { data } = this.state
+
     return (
 
       <div className="App">
-       <MyLabel text={this.state.labelText} outroText="1"></MyLabel>
- 
-       <MyButton handleClick={this.setLabelText} label="Botão 1"></MyButton>
-       <MyButton handleClick={this.setLabelText} label="Botão 2"></MyButton>
-       <MyButton handleClick={this.setLabelText} label="Botão 3"></MyButton>
-       <MyButton handleClick={this.setLabelText} label="Botão 4"></MyButton>
- 
+          <ul>
+            {data.map(item => <li>{item.full_name}</li>)}
+          </ul>
       </div> 
  
    );
